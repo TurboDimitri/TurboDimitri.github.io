0e62d32a24e33bef11d998921798a5d461333eb0
yasmaih <turbodimitri@dawn>
2025-04-18 15:24:21 +0200

    aaa

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'appname' title`, () => {
+  it(`should have the 'turbodimitri' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('appname');
+    expect(app.title).toEqual('turbodimitri');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, appname');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, turbodimitri');
   });
 });
